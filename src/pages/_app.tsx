c796f9dfadc1a1cff2aac420698b8f7f6787ff9e
Will White <wrwwhite@gmail.com>
2024-05-29 08:43:11 -0400

    remove dead code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,9 @@ import { type AppType } from 'next/app'
 import { api } from '~/utils/api'
 
 import '~/styles/globals.css'
-//comment to make sure I can commit to github
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component className='text-primary' {...pageProps} />
 }
 
 export default api.withTRPC(MyApp)
-
-// test

c889c8803d3a1289a51798045f9f5d4bcfb7566e
Will White <wrwwhite@gmail.com>
2024-06-27 12:02:44 -0400

    add analytics package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import { type AppType } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
 
 import { api } from '~/utils/api'
 
 import '~/styles/globals.css'
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component className='text-primary' {...pageProps} />
+  return (
+    <>
+      <Analytics />
+      <Component className='text-primary' {...pageProps} />
+    </>
+  )
 }
 
 export default api.withTRPC(MyApp)
